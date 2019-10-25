26351c95603fbdaa507cdd6df96e524ca5fb199a
hibiking-0422 <50702357+hibiking-0422@users.noreply.github.com>
2019-10-25 17:33:44 +0900

    aaaaaa

--- php_install.sh
+++ php_install.sh
@@ -22,3 +22,89 @@ sudo firewall-cmd --reload
 
 #git install
 sudo yum install -y git
+
+#日本語化
+sudo localectl set-locale LANG=ja_JP.UTF-8
+
+#firewall
+systemctl start firewalld
+sudo firewall-cmd --permanent --zone=public --add-service=http
+sudo firewall-cmd --reload
+
+#apach install
+sudo yum install -y httpd
+sudo systemctl enable httpd.service
+
+#php install
+sudo yum install -y epel-release
+sudo yum -y install http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+yum list
+
+sudo yum install -y --enablerepo=remi-php73 php php-gd php-intl php-json php-mysqlnd php-mbstring php-xml
+php -m | grep -e intl -e mbstring -e SimpleXML
+
+#mysql
+#mysql
+sudo yum remove -y mariadb-libs
+sudo yum localinstall -y https://dev.mysql.com/get/mysql80-community-release-el7-2.noarch.rpm
+sudo yum install -y mysql-community-server
+sudo systemctl start mysqld
+sudo systemctl enable mysqld
+
+#mysql -v 0.5.2
+#mysql
+sudo yum remove -y mariadb-libs
+sudo yum localinstall -y https://dev.mysql.com/get/mysql80-community-release-el7-2.noarch.rpm
+sudo yum install -y mysql-community-server
+sudo systemctl start mysqld
+sudo systemctl enable mysqld
+
+
+#mysql
+sudo yum remove -y mariadb-libs
+sudo yum localinstall -y https://dev.mysql.com/get/mysql80-community-release-el7-2.noarch.rpm
+sudo yum install -y mysql-community-server
+sudo systemctl start mysqld
+sudo systemctl enable mysqld
+
+#mysql -v 0.5.2
+sudo yum install -y mysql-devel
+
+
+<<commentout
+
+-----------php初期設定-----------------
+1.php.iniを当リポジトリのものに変更
+
+2. /etc/httpd/conf/httpd.conf
+以下のAllowOverrideをAllに書き換える。
+<Directory "/var/www/html">
+   AllowOverride All
+
+
+
+
+-----------  mysql初期設定手順 -----------
+$grep password /var/log/mysqld.log
+-初期パスワードを確認
+$mysql -u root -p
+-さっきのパスワードを入力してログイン
+mysql>ALTER USER 'root'@'localhost' IDENTIFIED BY '[好きなパスワード]';
+-rootユーザのパスワードの再設定
+mysql>exit
+$mysql -u root -p
+-新しいパスワードで再度ログイン
+mysql>create user '[ユーザ名]'@'[ホスト名]' identified by '[パスワード]';
+#新規ユーザの作成
+mysql>grant all on *.* to '[ユーザ名]'@'[ホスト名]';
+#ユーザに権限付与(とりあえず全部与えとく)
+
+commentout
+
+
+
+
+
+
+
+
